f33fa57e477a2f17c3beaa784e844ab91cab3903
JoshCodedit <joshua.dixoncms@gmail.com>
2025-07-13 20:59:39 +0100

    presentations page

---
+++ src/pages/Presentations.jsx
@@ -0,0 +1,178 @@
+import Footer from "../components/Footer";
+
+export default function Presentations() {
+  return (
+    <div>
+      <div className="text-primaryGreen max-w-4xl mx-auto px-4 py-12">
+        <h2 className="section-header mb-10">Presentations</h2>
+        <h3 className="font-bold text-lg mb-6">Keynote</h3>
+        <ul className="mb-12 space-y-4 main-text">
+          <li>
+            <span className="font-bold">
+              British Popular Culture(s) Conference
+            </span>
+            , June 2025, Falmouth University
+          </li>
+          <li>
+            <span className="font-bold">Inaugural SLRC Annual Lecture</span>,
+            April 2023, Stephen Lawrence Research Centre, De Montfort University
+          </li>
+          <li>
+            <span className="font-bold">
+              Black British Music: Sacred and Secular
+            </span>
+            , March 2023, Department of Music, University of Bristol
+          </li>
+          <li>
+            <span className="font-bold">
+              5th Annual Ethnographies of Crime &amp; Control Symposium
+            </span>
+            , King’s College, University of Cambridge, September 2022
+          </li>
+          <li>
+            <span className="font-bold">Annual Black Studies Lecture</span>,
+            University of Nottingham, January 2021
+          </li>
+        </ul>
+        <h3 className="font-bold text-lg mb-6">Guest Speaker</h3>
+        <ul className="mb-12 space-y-4 main-text">
+          <li className="font-bold text-primaryOrange">2024</li>
+          <li>
+            <span className="font-bold">
+              Cardiff University School of English, Communication and Philosophy
+            </span>
+            : ‘Saying something? Rap, grime and UK drill as a mode of social
+            commentary’
+          </li>
+          <li>
+            <span className="font-bold">
+              ELBWO (East London Black Women’s Organisation)
+            </span>
+            : Black History Month
+          </li>
+          <li>
+            <span className="font-bold">
+              TORCH University of Oxford Race Reflections seminar
+            </span>
+            : ‘Like Lockdown Never Happened: Music and Culture during COVID’
+          </li>
+          <li>
+            <span className="font-bold">
+              Louder than Words Festival – Manchester
+            </span>
+            : In conversation with Professor Neil Fox
+          </li>
+          <li>
+            <span className="font-bold">Newham Festival of Stories</span>: East
+            Ham Library
+          </li>
+          <li>
+            <span className="font-bold">University of Birmingham</span>:
+            Professor Patricia Noxolo hosts discussion with Dr. Joy White
+          </li>
+          <li>
+            <span className="font-bold">
+              Caribbean Social Forum - National Maritime Museum
+            </span>
+            : ‘Growing up under the influence: A sonic genealogy of Grime’
+          </li>
+          <li>
+            <span className="font-bold">
+              Metal's Future Station – Liverpool
+            </span>
+            : ‘Like Lockdown Never Happened: Music and Culture during COVID’
+          </li>
+          <li>
+            <span className="font-bold">London Interdisciplinary School</span>:
+            Applied Ethnography, Practical Ethics
+          </li>
+          <li className="font-bold text-primaryOrange">2023</li>
+          <li>
+            <span className="font-bold">
+              ISRF Climate Crisis Conference Bologna
+            </span>
+            : ‘Terraformed: A hyperlocal story of climate change, inequality and
+            gentrification’
+          </li>
+          <li>
+            <span className="font-bold">Healing Justice</span>: Panellist,
+            Rehumanising the Ends – London
+          </li>
+          <li>
+            <span className="font-bold">Metro54</span>: Panellist, Sonic
+            Intimacies conference - Amsterdam
+          </li>
+          <li className="font-bold text-primaryOrange">2022</li>
+          <li>
+            <span className="font-bold">
+              UCL Urban Laboratories: State of the Legacy conference
+            </span>
+            : Terraformed: Young Black Lives in the Inner City (December)
+          </li>
+          <li>
+            <span className="font-bold">
+              ICA Sistrens in the Struggle: Black Women in Britain – A Symposium
+            </span>
+            : Terraformed: Young Black Lives in the Inner City (August){" "}
+            <a
+              href="https://www.youtube.com/watch?v=jOO2hRBtMlw"
+              className="text-primaryOrange hover:underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              (video)
+            </a>
+          </li>
+          <li>
+            <span className="font-bold">
+              University of Westminster - School of Social Sciences
+            </span>
+            : Terraformed: Young Black Lives in the Inner City
+          </li>
+          <li>
+            <span className="font-bold">
+              University of Westminster - Music Cities Module
+            </span>
+            : Crossing Borders: The Social, Economic and Cultural Significance
+            of Grime (February)
+          </li>
+          <li>
+            <span className="font-bold">
+              Kings College, London BA Social Sciences Seminar series
+            </span>
+            : Crossing Borders: The Social, Economic and Cultural Significance
+            of Grime
+          </li>
+          <li className="font-bold text-primaryOrange">2021</li>
+          <li>
+            <span className="font-bold">
+              University of York Music Research Seminar Department of Music
+            </span>
+            : Crossing Borders: The Social, Economic and Cultural Significance
+            of Grime (October)
+          </li>
+          <li className="font-bold text-primaryOrange">2019</li>
+          <li>
+            <span className="font-bold">
+              UCL Urban Lab: Cities Imaginaries / Performance Lab
+            </span>
+            : Crossing Borders: The Social, Economic and Cultural Significance
+            of Grime
+          </li>
+          <li className="font-bold text-primaryOrange">2017</li>
+          <li>
+            <span className="font-bold">
+              Weekend of Global Cultures: Urban Culture and Political Engagement
+            </span>
+            : Tropenmuseum, Amsterdam
+          </li>
+          <li>
+            <span className="font-bold">Manchester University</span>: Hip Hop,
+            Spoken Word and the Library 'Reflections on a decade of Grime'
+          </li>
+        </ul>
+      </div>
+      <Footer />
+    </div>
+  );
+}
